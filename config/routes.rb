20373eae03d209a2aa29a89e9f8f75602d63404e
TimerYao <1129249909@qq.com>
2018-06-19 14:51:42 +0800

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   
   get '/signup', to: 'users#new'
-
+  
+  resources :users
   # root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
