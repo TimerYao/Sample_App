78698cf74554a52d8923d6b2a663881a7a0d7b92
TimerYao <1129249909@qq.com>
2018-06-21 12:33:56 +0800

    account-email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
-    before_save { self.email = email.downcase }
+    attr_accessor :remember_token, :activation_token
+    # before_save { self.email = email.downcase }
+    before_save :downcase_email
+    before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
     # validates(:name, presence: true)
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -38,5 +40,17 @@ class User < ApplicationRecord
     #  忘记用户
     def forget
         update_attribute(:remember_digest, nil)
+    end
+
+    private
+      #    电子邮箱转换为小写
+    def downcase_email
+        # self.email = email.downcase
+        email.downcase!
+    end 
+    # 创建并赋值激活令牌和摘要
+    def create_activation_digest
+        self.activation_token = User.new_token
+        self.activation_digest = User.digest(activation_token)
     end
 end
